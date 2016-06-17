110480f9974cee3e972ada1e3726a420cd73bf35
ThunderDeliverer <jan.turk12@gmail.com>
2016-06-17 17:31:44 +0200

    Test push

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Jan Turk on 15/06/16.
+//  Copyright © 2016 Jan Turk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: AnyObject) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+    
+}
+
