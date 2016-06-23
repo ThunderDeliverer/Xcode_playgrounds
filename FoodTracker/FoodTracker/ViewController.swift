bc6c0442a2ffce212ce0fa2811b7c310b5b73f3c
ThunderDeliverer <jan.turk12@gmail.com>
2016-06-24 00:28:46 +0200

    Building te UI: Implement a Custom Control completed. UI could use some fine tuning, but everything works the way it was supposed to.

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     
     override func viewDidLoad() {
@@ -66,9 +67,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         
         presentViewController(imagePickerController, animated: true, completion: nil)
-    }
-    @IBAction func setDefaultLabelText(sender: AnyObject) {
-        mealNameLabel.text = "Default Text"
     }
     
     
